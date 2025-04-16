11333781708bb9eb67a33e83d90230db862ed800
Mahesh Lalwani <mahesh.lalwani59@gmail.com>
2025-04-17 02:51:32 +0530

    Delete Smartworking/CustomOrderProcessing directory

--- Smartworking/CustomOrderProcessing/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Smartworking_CustomOrderProcessing',
-    __DIR__
-);
